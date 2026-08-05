b12b215358f6e50b8c551058093fccdc56e30d6c
Sungpyo Hong <sungpyo9053@kookmin.ac.kr>
2026-08-05 22:42:58 +0900

    Prevent UTF-8 summary values from being stripped

--- deploy/wordpress/huntlab-article-toc/huntlab-article-toc.php
+++ deploy/wordpress/huntlab-article-toc/huntlab-article-toc.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HuntLab Article Table of Contents
  * Description: Adds a warm, accessible H2/H3 table of contents to HuntLab posts.
- * Version: 1.1.4
+ * Version: 1.1.5
  * Author: HuntLab
  */
 
@@ -22,14 +22,17 @@ function huntlab_article_summary_text( $html, $limit = 190 ) {
 	if ( '' === $text ) {
 		return '';
 	}
-	if ( function_exists( 'mb_strlen' ) && mb_strlen( $text, 'UTF-8' ) > $limit ) {
-		return rtrim( mb_substr( $text, 0, $limit, 'UTF-8' ) ) . '…';
+	if ( function_exists( 'mb_strlen' ) ) {
+		if ( mb_strlen( $text, 'UTF-8' ) > $limit ) {
+			return rtrim( mb_substr( $text, 0, $limit, 'UTF-8' ) ) . '…';
+		}
+		return $text;
 	}
-	if ( ! function_exists( 'mb_strlen' ) && preg_match_all( '/./us', $text, $characters ) && count( $characters[0] ) > $limit ) {
-		return rtrim( implode( '', array_slice( $characters[0], 0, $limit ) ) ) . '…';
-	}
-	if ( strlen( $text ) > $limit ) {
-		return rtrim( substr( $text, 0, $limit ) ) . '…';
+	if ( preg_match_all( '/./us', $text, $characters ) ) {
+		if ( count( $characters[0] ) > $limit ) {
+			return rtrim( implode( '', array_slice( $characters[0], 0, $limit ) ) ) . '…';
+		}
+		return $text;
 	}
 	return $text;
 }
